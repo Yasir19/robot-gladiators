551c44c7232c61e1243191d6349238523a094688
yasir19 <yasir.habboo@gmail.com>
2021-11-14 16:29:40 -0600

    add console.log to the varplayerName

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,12 @@
-window.alert("This is an alert! JavaScript is running!");
+// This creates function named "fight"//
+function fight(){
+    window.alert("The fight has begun!");
+}
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log("the logs a string");
+//this will do math and log 20
+console.log(10+10);
+// what is this?
+console.log("our robot's name is " + playerName );
+//fight();
